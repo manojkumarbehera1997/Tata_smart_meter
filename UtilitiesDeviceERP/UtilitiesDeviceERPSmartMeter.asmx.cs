fbcccc821b89aae38dc73c1d26640e8ab0152f74
jajati <jajatibadu@gmail.com>
2021-10-06 18:58:56 +0530

    added database connection

--- UtilitiesDeviceERP/UtilitiesDeviceERPSmartMeter.asmx.cs
+++ UtilitiesDeviceERP/UtilitiesDeviceERPSmartMeter.asmx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
@@ -18,14 +21,43 @@ namespace UtilitiesDeviceERP
     // [System.Web.Script.Services.ScriptService]
     public class UtilitiesDeviceERPSmartMeter : System.Web.Services.WebService
     {
+        public string query, constr;
+        public SqlConnection con;
+        public void connection()
+        {
+            constr = ConfigurationManager.ConnectionStrings["Tata"].ToString();
+            con = new SqlConnection(constr);
+            con.Open();
+        }
+
         [SoapDocumentMethod(OneWay = true)]
         [WebMethod]
         public void UtilitiesDeviceERPSmartMeterCreateRequest_Out(abc.UtilsDvceERPSmrtMtrCrteReqMsg utilitiesDeviceERPSmartMeterCreateRequest)
         {
-            //utilitiesDeviceERPSmartMeterCreateRequest.MessageHeader.UUID
-            //do stuffs...
-
-
+            connection();
+            query = "Insert_Equipment";
+            SqlCommand com = new SqlCommand(query, con);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@ID", 3);
+            com.Parameters.AddWithValue("@Utility_ID ", utilitiesDeviceERPSmartMeterCreateRequest.UtilitiesDevice.ID);
+            com.Parameters.AddWithValue("@Meter_Class ", null);
+            com.Parameters.AddWithValue("@Manufacturer_ID", null);
+            com.Parameters.AddWithValue("@Status ", null);
+            com.Parameters.AddWithValue("@Meter_ID ", utilitiesDeviceERPSmartMeterCreateRequest.UtilitiesDevice.SerialID);
+            com.Parameters.AddWithValue("@Device_Type", null);
+            com.Parameters.AddWithValue("@Model_ID ", null);
+            com.Parameters.AddWithValue("@StartDate ", utilitiesDeviceERPSmartMeterCreateRequest.UtilitiesDevice.StartDate);
+            com.Parameters.AddWithValue("@EndDate", utilitiesDeviceERPSmartMeterCreateRequest.UtilitiesDevice.EndDate);
+            com.Parameters.AddWithValue("@Material_ID ", utilitiesDeviceERPSmartMeterCreateRequest.UtilitiesDevice.MaterialID);
+            com.Parameters.AddWithValue("@Name_Plate_ID ", utilitiesDeviceERPSmartMeterCreateRequest.UtilitiesDevice.SerialID);
+            com.Parameters.AddWithValue("@HES_ID", null);
+            com.Parameters.AddWithValue("@Data_Source ", null);
+            com.Parameters.AddWithValue("@Org_ID ", null);
+            com.Parameters.AddWithValue("@Created_by", null);
+            com.Parameters.AddWithValue("@Created_on ", null);
+            com.Parameters.AddWithValue("@changed_by ", null);
+            com.Parameters.AddWithValue("@changed_on ", null);
+            com.ExecuteNonQuery();
         }
 
 
