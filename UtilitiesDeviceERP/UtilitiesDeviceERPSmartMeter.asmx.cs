f966da5f1820dd723a3b3012dbd450b9873166fd
Sobhan Panda <sobhan@centroxy.com>
2021-10-06 15:11:32 +0530

    Web reference

--- UtilitiesDeviceERP/UtilitiesDeviceERPSmartMeter.asmx.cs
+++ UtilitiesDeviceERP/UtilitiesDeviceERPSmartMeter.asmx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Web.Services.Protocols;
 using UtilitiesDeviceERP.Models;
 
 namespace UtilitiesDeviceERP
@@ -10,17 +11,31 @@ namespace UtilitiesDeviceERP
     /// <summary>
     /// Summary description for UtilitiesDeviceERPSmartMeterCreateRequest_Out
     /// </summary>
-    [WebService(Namespace = "http://tempuri.org/")]
+    [WebService(Namespace = "http://tpddl.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
     public class UtilitiesDeviceERPSmartMeter : System.Web.Services.WebService
     {
+        [SoapDocumentMethod(OneWay = true)]
+        [WebMethod]
+        public void UtilitiesDeviceERPSmartMeterCreateRequest_Out(abc.UtilsDvceERPSmrtMtrCrteReqMsg utilitiesDeviceERPSmartMeterCreateRequest)
+        {
+            //utilitiesDeviceERPSmartMeterCreateRequest.MessageHeader.UUID
+            //do stuffs...
+
+
+        }
+
 
         [WebMethod]
-        public string UtilitiesDeviceERPSmartMeterCreateRequest_Out(UtilitiesDeviceERPSmartMeterCreateRequest utilitiesDeviceERPSmartMeterCreateRequest)
+        public string UtilitiesDeviceERPSmartMeterCreateRequest_In()
         {
+
+            //do stuffs...
+
+
             return "Hello World";
         }
     }
